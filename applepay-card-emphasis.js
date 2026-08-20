f08cc112c12343c9824e3bad573b427357ec70ba
tz4hj797t6-cmyk <tz4hj797t6@privaterelay.appleid.com>
2026-08-20 12:49:22 -0400

    Bump Transak checkout asset version

--- applepay-card-emphasis.js
+++ applepay-card-emphasis.js
@@ -6,7 +6,7 @@
   // This file is already included by index.html, so no markup change is needed.
   if (!document.querySelector('script[data-nxt-transak]')) {
     const script = document.createElement('script');
-    script.src = '/transak-checkout.js?v=20260820-1';
+    script.src = '/transak-checkout.js?v=20260820-2';
     script.async = false;
     script.dataset.nxtTransak = '1';
     document.body.appendChild(script);
